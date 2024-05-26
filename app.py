45c6c15390fda71c2998d3a244db30be09a8b880
Onneqq <adampenno93@gmail.com>
2024-05-26 16:06:04 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([{
+        'id': task.id,
+        'title': task.title,
+        'description': task.description,
+        'completed': task.completed
+    } for task in tasks])
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'], description=data.get('description'))
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'id': new_task.id})
+
+@app.route('/tasks/<int:task_id>', methods=['PATCH'])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    data = request.get_json()
+    if 'title' in data:
+        task.title = data['title']
+    if 'description' in data:
+        task.description = data['description']
+    if 'completed' in data:
+        task.completed = data['completed']
+    db.session.commit()
+    return jsonify({'id': task.id})
+
+if __name__ == '__main__':
+    app.run(debug=True)
